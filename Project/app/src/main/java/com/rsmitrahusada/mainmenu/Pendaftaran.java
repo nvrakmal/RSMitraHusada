11c9fef63f69b5856655b0de41a963f98e7f44d4
nvrakmal <nvrakmal5@gmail.com>
2023-06-19 15:41:23 +0700

    master: mengisi activity pendaftaran dan mengubah warna tema aplikasi

--- Project/app/src/main/java/com/rsmitrahusada/mainmenu/Pendaftaran.java
+++ Project/app/src/main/java/com/rsmitrahusada/mainmenu/Pendaftaran.java
@@ -3,11 +3,17 @@ package com.rsmitrahusada.mainmenu;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageButton;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.rsmitrahusada.R;
+
 public class Pendaftaran extends AppCompatActivity {
 
     private ImageButton backButton;
+    private WebView webView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +27,35 @@ public class Pendaftaran extends AppCompatActivity {
                 onBackPressed(); // Panggil fungsi onBackPressed() saat tombol kembali ditekan
             }
         });
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://103.126.172.66/pendaftaran-online-web/login");
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload(); // Muat ulang halaman web saat melakukan refresh
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                swipeRefreshLayout.setRefreshing(false); // Menghentikan animasi refresh setelah halaman selesai dimuat
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        finish(); // Kembali ke aktivitas sebelumnya
+        if (webView.canGoBack()) {
+            webView.goBack(); // Kembali ke halaman sebelumnya jika ada
+        } else {
+            super.onBackPressed();
+            finish(); // Kembali ke aktivitas sebelumnya
+        }
     }
-
 }
